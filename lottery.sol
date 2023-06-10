978fa1279c7909b61ecf826e5dc5f9735d592667
Rajiv S <rajiv620019@gmail.com>
2023-06-10 20:01:56 +0530

    Wrote participate function

--- lottery.sol
+++ lottery.sol
@@ -2,4 +2,20 @@
 
 pragma solidity 0.9;
 
-contract Lottery {}
+contract Lottery {
+    // Defining magager, players and winner
+    address public manager;
+    address payable[] public players;
+    address payable public winner;
+
+    // Setting up manager address
+    Constructor () {
+        manager = msg.sender;
+    }
+
+    // Function to participate
+    function participate() public payable () {
+        require (msg.value == 1 ether, "Please pay ony 1 Ether only");
+        players.push(payable(msg.sender));
+    }
+}
